b4a985e9eb6baef02b80e9f810402ee024e90ac8
Juan F Garcia <juanfgarciacas@gmail.com>
2020-10-10 20:00:56 +0200

    First commit

---
+++ Computer16Bit.vhd
@@ -0,0 +1,23 @@
+--Libraries
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+--Definition if the entity
+entity Computer16Bit is
+	port(
+		clk: in std_logic;
+		rst: in std_logic);
+end Computer16Bit;
+
+--Architecture
+architecture Behavioral of Computer16Bit is
+begin
+
+--cpu : entity work.cpu
+	--port map();
+
+--VGA controller
+--Keyboard driver
+--Main clock process
+--Clock divisors processes
+end Behavioral;
